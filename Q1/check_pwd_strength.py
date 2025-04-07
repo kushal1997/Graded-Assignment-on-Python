0b52dddd7332e85065636157a7274ef38eff2eee
kushal1997 <kushalrao103@gmail.com>
2025-04-07 23:19:35 +0530

    same with predefined function

--- Q1/check_pwd_strength.py
+++ Q1/check_pwd_strength.py
@@ -7,42 +7,49 @@ def check_password_strength(pwd):
         print("1) Password must be 8 chars long")
         return False
     
-    check_uppercase_letters = False
-    check_smallcase_letters = False
+    check_uppercase_letters = check_smallcase_letters =  check_numbers = check_special_chars = False
 
-    check_numbers = False
+ # ================== WITH BRUTE-FORCE TECHNIQUE =============================
+    # for ch in pwd:
+    #     if ch >= 'A' and ch <= 'Z':
+    #         check_uppercase_letters = True
+    #     elif ch >= 'a' and ch <= 'z':
+    #         check_smallcase_letters = True
+    #     elif ch >= '0' and ch <= '9':
+    #         check_numbers = True
+    #     else:
+    #         check_special_chars = True
 
-    check_special_chars = False
-
-    for ch in pwd:
-        if ch >= 'A' and ch <= 'Z':
+# ==================== WITH PREDIFINED FUNCTIONS =============================
+    for char in pwd:
+        if char.isupper():
             check_uppercase_letters = True
-        elif ch >= 'a' and ch <= 'z':
+        elif char.islower():
             check_smallcase_letters = True
-        elif ch >= '0' and ch <= '9':
+        elif char.isdigit():
             check_numbers = True
-        else:
+        elif not char.isalnum():
             check_special_chars = True
-    
+
     if check_uppercase_letters:
         print("2) Contains uppercase letter")
     else:
-        print("2) Missing uppercase letter")
+        print("\n 2) Missing uppercase letter")
     
     if check_smallcase_letters:
         print("3) Contains lowercase letter")
     else:
-        print("3) Missing lowercase letter")
+        print("\n 3) Missing lowercase letter")
 
     if check_numbers:
         print("4) Contains digit")
     else:
-        print("4) Missing digits")
+        print("\n 4) Missing digits")
     
     if check_special_chars:
         print("5) Contains special characters")
     else:
-        print("5) Missing special character")
+        print("\n 5) Missing special character")
     
     return check_uppercase_letters and check_smallcase_letters and check_numbers and check_special_chars
 
