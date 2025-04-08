bc732028ef821d48602ee08d6ac56bc1fb253053
kushal1997 <kushalrao103@gmail.com>
2025-04-08 08:15:09 +0530

    WIP : using try & except

--- Q2/monitor_cpu.py
+++ Q2/monitor_cpu.py
@@ -3,10 +3,13 @@ import psutil
 
 def monitor_cpu_usage(threshold):
     print("Monitoring CPU usage... \n")
-    while True:
-        cpu_usage = psutil.cpu_percent(interval=0.5)
+    try:
+        while True:
+            cpu_usage = psutil.cpu_percent(interval=0.5)
 
-        if cpu_usage > threshold :
-            print(f'Alert! CPU usage exceeds threshold: {cpu_usage}%')
+            if cpu_usage > threshold :
+                print(f"Alert! CPU usage exceeds threshold: {cpu_usage}%")
+    except Exception as e:
+        print(f"Error occured : {e}")
 
 monitor_cpu_usage(8)
