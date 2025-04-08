13734163c10210786b00e6650a9bf40a7ebc0b52
kushal1997 <kushalrao103@gmail.com>
2025-04-08 08:40:27 +0530

    WIP : handled another error while stoping

--- Q2/monitor_cpu.py
+++ Q2/monitor_cpu.py
@@ -9,6 +9,10 @@ def monitor_cpu_usage(threshold):
 
             if cpu_usage > threshold :
                 print(f"Alert! CPU usage exceeds threshold: {cpu_usage}%")
+                
+    except KeyboardInterrupt:
+        print("\nMonitoring stopped by user.")
+
     except Exception as e:
         print(f"Error occured : {e}")
 
