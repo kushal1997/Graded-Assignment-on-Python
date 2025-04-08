798b120db8ad1ad12594a06e7890800926ebd35c
kushal1997 <kushalrao103@gmail.com>
2025-04-08 15:15:40 +0530

    WIP : used try & except

--- Q3/read_config.py
+++ Q3/read_config.py
@@ -2,17 +2,25 @@
 config_data = {}
 section = ''
 
-with open(".config",'r') as file:
-    for line in file:
-        # print(line)
-        if line.startswith("["):
-            # print(line[1:-2])
-            section = line[1:-2]
-            config_data[section] = {}
-        elif '=' in line:
-            # print(line.split("="))
-            key, value = line.split("=")
-            print("key,value",key,value)
-            config_data[section][key]=value[:-1]
+try:
+    with open(".config",'r') as file:
+        for line in file:
+            # print(line)
+            if not line:
+                continue
+            if line.startswith("["):
+                # print(line[1:-2])
+                section = line[1:-2]
+                config_data[section] = {}
+            elif '=' in line:
+                if section:
+                    # print(line.split("="))
+                    key, value = line.split("=")
+                    print("key,value",key,value)
+                    config_data[section][key]=value[:-1]
+            else:
+                print("Wrong line format",line)
+except Exception as e:
+    print("error : ",e)
 
 print("config data",config_data)
